21791f59b240fa30f2d2f6fbb5f8220c4ab3798e
Aman Gill <kgb_operative@riseup.net>
2013-11-12 08:49:09 -0800

    temperary save to move machines

--- fsaTest.hs
+++ fsaTest.hs
@@ -1,7 +1,7 @@
 import Data.Char
 import Data.Set 
-import System.IO
 import Data.Tree
+import System.IO
 
 -- create a transition table based on the value of the divisor
 getDFAtrans :: Int -> [Int] -> [Int] -> Set (Int, Int, Set Char)
@@ -15,19 +15,9 @@ getDFAtrans k [a] (c:cs) =
 getDFAtrans k (a:as) (c:cs) = 
     (getDFAtrans k [a] (c:cs)) `union` (getDFAtrans k as (c:cs))
 
-{-- old, list verion
-getDFAtrans :: Int -> [Int] -> [Int] -> [(Int, Int, [Char])]
-getDFAtrans k [a] [c] = [(a, (a * 10 + c) `mod` k, [intToDigit c])
-                       , (a + k, ((a * 10 + c) `mod` k) + k, [intToDigit c])]
-getDFAtrans k [a] (c:cs) = (a, (a * 10 + c) `mod` k, [intToDigit c])
-                       : (a + k, ((a * 10 + c) `mod` k) + k, [intToDigit c]) 
-                       : (getDFAtrans k [a] cs)
-getDFAtrans k (a:as) (c:cs) = (getDFAtrans k [a] (c:cs)) ++ (getDFAtrans k as (c:cs))
---}
-
 -- create a list of tuples for the dropped digit transitions
-getDropTrans :: Int -> Set ((Int, Int, [Int]))
-getDropTrans a = 
+getNFATrans :: Int -> Set ((Int, Int, [Int]))
+getNFATrans a = 
     fromList $ zip3 [0..(a-1)] [a..(2*a-1)] (rep [0..9] a)
 
 -- replicate list a n number of times
@@ -38,36 +28,29 @@ rep a n =
 
 -- convert a list of Ints to a list of Chars
 intsToDigits :: [Int] -> Set Char
-intsToDigits [a] = singleton (intToDigit a)
+intsToDigits [] = empty
 intsToDigits (a:as) = 
-    singleton (intToDigit a) `union` (intsToDigits as)
+    if Data.Set.null x then singleton (intToDigit a)
+    else singleton (intToDigit a) `union` x
+        where x = intsToDigits as
 
 -- convert the transition table from the dropped-digit tuples
 convertTransSet :: Set (Int, Int, [Int]) -> Set (Int, Int, Set Char)
 convertTransSet a = 
-    if Data.Set.null a then empty
+    if Data.Set.size a == 1 then singleton (convertTrans (findMin a))
     else singleton (convertTrans (findMin a)) `union` convertTransSet (deleteMin a)
         where convertTrans (a, b, c) = (a, b, intsToDigits c)
+
+-- merge transitions that have the same initial and final state
+mergeDelta :: Ord a => (a, a, Set b) -> (a, a, Set b) -> Set (a, a, Set b)
+mergeDelta (a1, b1, c1) (a2, b2, c2) =
+    if a1 == a2 && b1 == b2 then singleton (a1, b1, (c1 `union` c2))
+    else singleton (a1, b1, c1) `union` singleton (a2, b2, c2)
 
 -- create a list of final accepting states
 getFinals :: Int -> Set Int
 getFinals k = 
     singleton 0 `union` singleton k
 
-{-- combineDelta :: (a, b, [c]) -> (a, b, [c]) -> (a, b, [c])
--- combineDelta (a1, b1, (
--- reduce the number of transitions
-reduceDelta :: [(Int, Int, [Char])] -> [(Int, Int, [Char])]
-reduceDelta [(a, b, (c:cs))] = 
-
--- define NFA as a data type
-data NFA = NFA { states :: [Int]
-               , sigma :: [Char]
-               , delta :: [(Int, Int, [Char])] 
-               , initState :: Int 
-               , finalState :: [Int] 
-               }
---}
-main :: IO()
 main =
-    putStrLn $ showTreeWith False True $ (getDFAtrans 3 [0..2] [0..9]) `union` convertTransSet (getDropTrans 3)
+    putStrLn $ showTreeWith False True $ (getDFAtrans 2 [0,1] [0..9]) `union` convertTransSet (getNFATrans 2)
